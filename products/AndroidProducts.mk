15af85860b31e5c0868063b8a63da3342deca6e9
Joe-Tech <josephodney@gmail.com>
2015-11-12 23:01:47 -0500

    Add New Nexus 6P

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,2 +1,5 @@
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/coldfusion_shamu.mk
+     $(LOCAL_DIR)/coldfusion_angler.mk \
+
+
--- products/common_fingerprints.mk
+++ products/common_fingerprints.mk
@@ -6,3 +6,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58R 2308909 release-keys"
 endif
 
+ifeq ($(TARGET_PRODUCT),coldfusion_angler)
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0/MDB08M/2353240:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0 MDB08M 2353240 release-keys"
+endif
